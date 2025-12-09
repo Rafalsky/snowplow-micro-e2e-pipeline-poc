93232510688bc09b0c4a3da5a556046a9507f8dc
Rafalsky <rafalsky1313@gmail.com>
2025-12-09 22:03:50 +0100

    Add JSON schemas for dealer events and configure Iglu resolver

--- web/track.js
+++ web/track.js
@@ -1,5 +1,3 @@
-// Import and initialize Snowplow Analytics tracker
-;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[]; p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments) };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1; n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","","snowplow"));
 
 //Implement the tracker
 window.snowplow('newTracker', 'sp', 'micro.localhost', {
@@ -17,3 +15,36 @@ window.snowplow('trackPageView');
 
 // Example of tracking a custom event
 window.snowplow('trackStructEvent', 'category', 'action', 'label', 'property', 'value');
+
+
+//react spa pageview
+(function() {
+  // Keep reference to the original functions
+  const pushState = history.pushState;
+  const replaceState = history.replaceState;
+
+  function track() {
+    if (window.snowplow) {
+      window.snowplow('trackPageView');
+    }
+  }
+
+  // Patch pushState
+  history.pushState = function(state, title, url) {
+    const result = pushState.apply(this, arguments);
+    track();
+    return result;
+  };
+
+  // Patch replaceState
+  history.replaceState = function(state, title, url) {
+    const result = replaceState.apply(this, arguments);
+    track();
+    return result;
+  };
+
+  // Back/forward button
+  window.addEventListener('popstate', track);
+
+  // First load already tracked manually
+})();
